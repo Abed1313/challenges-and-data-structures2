32a982764fc9a551eb5393b63d705d647c31c3d3
AbdelRadwan <21023979@student.ltuc.com>
2024-07-07 05:19:15 +0300

    Reverse Words

---
+++ Challenges/Reverse-Words/Reverse-Words-Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Reverse_Words_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Challenges/Reverse-Words/Reverse-Words/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reverse_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testCases = new string[]
+            {
+                "csharp is programming language",
+                "Reverse the words in this sentence",
+                "challenges and data structures"
+            };
+
+            foreach (var testCase in testCases)
+            {
+                string reversed = ReverseWords(testCase);
+                Console.WriteLine("Input: " + testCase);
+                Console.WriteLine("Output: " + reversed);
+                Console.WriteLine();
+            }
+        }
+
+        public static string ReverseWords(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+    }
+}
