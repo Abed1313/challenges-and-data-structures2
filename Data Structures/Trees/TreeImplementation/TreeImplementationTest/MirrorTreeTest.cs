7c5a33347c481699f535e9bd9182bac9caaad64e
AbdelRadwan <21023979@student.ltuc.com>
2024-08-15 23:04:49 +0300

    MirrorTreeTest

---
+++ Data Structures/Trees/TreeImplementation/TreeImplementationTest/MirrorTreeTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreeImplementation.MirrorTree;
+
+namespace TreeImplementationTest
+{
+    public class MirrorTreeTest
+    {
+        [Fact]
+        public void Test_MirroringTree_InorderTraversal()
+        {
+            // Arrange
+            var tree = new MirrorTree(5);
+            tree.Insert(3);
+            tree.Insert(7);
+            tree.Insert(2);
+            tree.Insert(4);
+            tree.Insert(6);
+            tree.Insert(8);
+
+            // Act
+            
+            var inOrderResult = tree.InOrderTraversal2(tree.Root);
+
+            // Assert
+            var expectedInOrder = new List<int> { 8, 7, 6, 5, 4, 3, 2 }; // This is the mirrored in-order traversal
+            Assert.Equal(expectedInOrder, inOrderResult);
+        }
+
+        [Fact]
+        public void TestSingleNodeTree()
+        {
+            // Arrange
+            var tree = new MirrorTree(10);
+
+            // Act
+            var inOrderResult = tree.InOrderTraversal2(tree.Root);
+
+            // Assert
+            var expectedInOrder = new List<int> { 10 };
+            Assert.Equal(expectedInOrder, inOrderResult);
+        }
+    }
+}
